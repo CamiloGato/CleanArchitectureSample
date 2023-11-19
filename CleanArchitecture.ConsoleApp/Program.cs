cf5c94eaa757eb902d0427cd09ef12b5fa77faf1
CamiloGato <cm.andres022@hotmail.com>
2023-11-19 18:50:53 -0500

    Creation of Queries

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -1,53 +1,67 @@
 ﻿
 using CleanArchitecture.Data;
 using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
 
 var builder = new StreamerDbContextFactory();
 await using var dbContext = builder.CreateDbContext(args);
 
+await AddNewRecords();
+QueryStreamers(dbContext);
+return;
 
-await dbContext?.SaveChangesAsync()!;
-
-var movies = new List<Video>()
+void QueryStreamers(StreamerDbContext dbcontext)
 {
-    new()
+    var videos = dbcontext.Videos?.Include(video => video.Streamer).ToList();
+    if (videos is null) return;
+    foreach (var video in videos)
     {
-        Name = "The Matrix",
-        Url = "https://www.netflix.com/watch/20557937",
-        StreamerId = 1,
-    },
-    new()
-    {
-        Name = "The Matrix Reloaded",
-        Url = "https://www.netflix.com/watch/60021228",
-        StreamerId = 1,
-    },
-    new()
-    {
-        Name = "The Matrix Revolutions",
-        Url = "https://www.netflix.com/watch/60024942",
-        StreamerId = 1,
-    },
-    new()
-    {
-        Name = "The Matrix",
-        Url = "https://www.primevideo.com/detail/The-Matrix/0GUR2L4N7JFVXZQYQYQYQYQYQY",
-        StreamerId = 2,
-    },
-    new()
-    {
-        Name = "The Matrix Reloaded",
-        Url = "https://www.primevideo.com/detail/The-Matrix-Reloaded/0GUR2L4N7JFVXZQYQYQYQYQYQY",
-        StreamerId = 2,
-    },
-    new()
-    {
-        Name = "The Matrix Revolutions",
-        Url = "https://www.primevideo.com/detail/The-Matrix-Revolutions/0GUR2L4N7JFVXZQYQYQYQYQYQY",
-        StreamerId = 2,
-    },
-};
+        Console.WriteLine($"Video: {video.Name}");
+        Console.WriteLine($"Streamer: {video.Streamer?.Name}");
+        Console.WriteLine($"Url: {video.Url}");
+        Console.WriteLine();
+    }
+}
 
-await dbContext?.Videos?.AddRangeAsync(movies)!;
-
-await dbContext?.SaveChangesAsync()!;
+async Task AddNewRecords()
+{
+    var streamer = new Streamer()
+    {
+        Name = "Disney",
+        Url = "https://www.disneyplus.com/",
+    };
+    
+    dbContext?.Streamers?.Add(streamer);
+    await dbContext?.SaveChangesAsync()!;
+    
+    var movies = new List<Video>()
+    {
+        new Video()
+        {
+            Name = "The Mandalorian",
+            Url = "https://www.disneyplus.com/series/the-mandalorian/3jLIGMDYINqD",
+            StreamerId = streamer.Id,
+        },
+        new Video()
+        {
+            Name = "WandaVision",
+            Url = "https://www.disneyplus.com/series/wandavision/4SrN28ZjDLwH",
+            StreamerId = streamer.Id,
+        },
+        new Video()
+        {
+            Name = "The Falcon and the Winter Soldier",
+            Url = "https://www.disneyplus.com/series/the-falcon-and-the-winter-soldier/4gglDBMx8icA",
+            StreamerId = streamer.Id,
+        },
+        new Video()
+        {
+            Name = "Loki",
+            Url = "https://www.disneyplus.com/series/loki/6pARMvILBGzF",
+            StreamerId = streamer.Id,
+        },
+    };
+    
+    await dbContext?.Videos?.AddRangeAsync(movies)!;
+    await dbContext?.SaveChangesAsync()!;
+}
